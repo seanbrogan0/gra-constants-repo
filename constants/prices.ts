cb85cbe3230b2f461823ee23d7fdf592379e260f
Sean Brogan <sean.brogan@hotmail.com>
2026-07-04 14:48:22 +0100

    removed price currency util error

--- constants/prices.ts
+++ constants/prices.ts
@@ -1,7 +1,8 @@
 // constants/prices.ts
 // Central registry of Grá pricing information. Change here to update everywhere.
 
-import { formatCurrency } from "@/lib/utils/currencyUtils";
+//import { formatCurrency } from "@/lib/utils/currencyUtils";
 
-export const EVENT_GOLD_PRICE = formatCurrency(30);
+//export const EVENT_GOLD_PRICE = formatCurrency(30);
 
+export const EVENT_GOLD_PRICE = (30);
