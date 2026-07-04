5cc019aab10edbf5d5350a9561428535b5967ab5
Sean Brogan <sean.brogan@hotmail.com>
2026-07-04 23:29:53 +0100

    0.1.7

--- constants/legalNames.ts
+++ constants/legalNames.ts
@@ -1,10 +1,10 @@
 // /lib/constants/legalNames.ts
 // Central registry of Grá legal names and addresses. Change here to update everywhere.
 
-export const EMAIL_PRIVACY = "privacy@gra.app";
-export const EMAIL_SUPPORT = "support@gra.app";
-export const EMAIL_LEGAL   = "legal@gra.app";
+export const EMAIL_PRIVACY = "privacy@londubhdesign.ie";
+export const EMAIL_SUPPORT = "support@londubhdesign.ie";
+export const EMAIL_LEGAL   = "legal@londubhdesign.ie";
 export const EXAMPLE_EMAIL = "example@email.ie";
-export const LEGAL_NAME = "Blackbird - TBC";
-export const LEGAL_ADDRESS = "TBC";
+export const LEGAL_NAME = "Lon Dubh Design";
+export const LEGAL_ADDRESS = "20 Bawnogue View, Kilkock, Kildare, Ireland";
 export const AI_PROVIDER = "TBC";
