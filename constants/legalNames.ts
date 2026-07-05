f0ab91883b9387b4d27a4d5f572199dce5c86d23
Sean Brogan <sean.brogan@hotmail.com>
2026-07-05 15:56:58 +0100

    new emails

--- constants/legalNames.ts
+++ constants/legalNames.ts
@@ -5,6 +5,6 @@ export const EMAIL_PRIVACY = "privacy@londubhdesign.ie";
 export const EMAIL_SUPPORT = "support@londubhdesign.ie";
 export const EMAIL_LEGAL   = "legal@londubhdesign.ie";
 export const EXAMPLE_EMAIL = "example@email.ie";
-export const LEGAL_NAME = "Lon Dubh Design";
+export const LEGAL_NAME = "Merula Labs";
 export const LEGAL_ADDRESS = "20 Bawnogue View, Kilkock, Kildare, Ireland";
 export const AI_PROVIDER = "TBC";
