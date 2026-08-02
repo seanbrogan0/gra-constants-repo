76d78c6d41530db16dcc426680a41a8f17e70142
Sean Brogan <sean.brogan@hotmail.com>
2026-08-02 19:03:23 +0100

    0.4.0

--- constants/legalNames.ts
+++ constants/legalNames.ts
@@ -1,9 +1,9 @@
 // /lib/constants/legalNames.ts
 // Central registry of Grá legal names and addresses. Change here to update everywhere.
 
-export const EMAIL_PRIVACY = "privacy@londubhdesign.ie";
-export const EMAIL_SUPPORT = "support@londubhdesign.ie";
-export const EMAIL_LEGAL   = "legal@londubhdesign.ie";
+export const EMAIL_PRIVACY = "privacy@TBC";
+export const EMAIL_SUPPORT = "support@TBC";
+export const EMAIL_LEGAL   = "legal@TBC";
 export const EXAMPLE_EMAIL = "example@email.ie";
 export const LEGAL_NAME = "Merula Labs";
 export const LEGAL_ADDRESS = "20 Bawnogue View, Kilkock, Kildare, Ireland";
